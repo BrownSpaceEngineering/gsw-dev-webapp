414a717d9c05ec42f8ebd702003c8631d3519e03
pppraveennn <59750868+pppraveennn@users.noreply.github.com>
2024-11-03 14:56:55 -0500

    added component for telemetry on frontend

--- pvd_webapp/client-webapp/src/App.tsx
+++ pvd_webapp/client-webapp/src/App.tsx
@@ -1,8 +1,12 @@
 import './App.css'
+import Telemetry from './components/telemetry';
 
 function App() { 
   return (
+    <>
     <h1 className="text-3xl font-bold underline">Filling in progress</h1>
+    <Telemetry />
+    </>
   )
 }
 
